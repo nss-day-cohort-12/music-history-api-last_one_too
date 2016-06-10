803fe5aee2364fc31f6ae7bb7fd71d6a12ed2f93
Theo Carnavos <tcarnavos@gmail.com>
2016-06-10 11:20:01 -0500

    added Models

---
+++ MusicHistoryAPI/src/MusicHistoryAPI/Models/Track.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicHistoryAPI.Models
+{
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public int AlbumId { get; set; }
+        public int GenreId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
--- MusicHistoryAPI/src/MusicHistoryAPI/Startup.cs
+++ MusicHistoryAPI/src/MusicHistoryAPI/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MusicHistoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MusicHistoryAPI
 {
@@ -17,38 +19,39 @@ namespace MusicHistoryAPI
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-            if (env.IsEnvironment("Development"))
-            {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
-            }
-
-            builder.AddEnvironmentVariables();
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=MusicHistoryDB;Trusted_Connection=True;";
+            services.AddDbContext<MusicHistoryContext>(options => options.UseSqlServer(connection));
 
+            // Add framework services.
             services.AddMvc();
+
+            // Allow any header and any method on a request from my local development machine
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowDevelopmentEnvironment",
+                    builder => builder
+                    // builder => builder.WithOrigins("http://localhost:8080") used to permit specific url
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
-            app.UseApplicationInsightsRequestTelemetry();
-
-            app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseMvc();
         }
